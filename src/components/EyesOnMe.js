c21c55fa343c014d58bcebcace3321bb6990dc9e
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-08-19 15:54:18 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
+import React from "react"
+
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+
+    function focusHandler(){
+
+        console.log("Good")
+    }
+  
+    function blurHandler(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+   
+    <button onFocus={focusHandler} onBlur={blurHandler}>Eyes on Me</button>
+  
+   ) 
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+
+    function passwordHandler(e){
+        e.preventDefault()
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+            <input type="password" onChange={passwordHandler} />
     )
 }
 
